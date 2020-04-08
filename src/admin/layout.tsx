890aacb51efaa30895799d7ba59221bdc4275e0f
unknown <m.khmalech@gmail.com>
2020-04-08 20:57:09 +0100

    add wrapper to admin layout

--- src/admin/layout.tsx
+++ src/admin/layout.tsx
@@ -1,5 +1,18 @@
 import * as React from "react";
+import * as Wrapper from "../commons/wrappers";
 export const AdminLayout = () => {
-  return <div>administartion</div>;
+  return (
+    <Wrapper.Container>
+      {/**
+      header goes here
+    */}
+      <Wrapper.Header>header </Wrapper.Header>
+
+      <Wrapper.BodyContainer>
+        <Wrapper.Sidebar>sidebar</Wrapper.Sidebar>
+        <Wrapper.Body>body</Wrapper.Body>
+      </Wrapper.BodyContainer>
+    </Wrapper.Container>
+  );
 };
-// hadi deja mssayba fe ittyni m9ada nicha tji deer route
+
--- src/commons/wrappers.ts
+++ src/commons/wrappers.ts
@@ -1,6 +1,26 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+// Container
+export const Container = styled("div")`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+//header wrapper
+export const Header = styled("div")``;
+
+// sidebar wrapper
+export const Sidebar = styled("div")`
+  main-width : 250px;
+`;
+
+// body wrapper
+export const Body = styled("main")`
+  display : flex;
+  flex-direction : row;
+`;
+export const BodyContainer = styled.div`
   margin-left: 300px;
   @media (max-width: 700px) {
     margin-left: 0px;
@@ -8,3 +28,6 @@ export const Container = styled.div`
 
   transition: all 0.3s cubic-bezier(0.075, 0.82, 0.165, 1);
 `;
+
+// footer wrapper
+export const Footer = styled("div")``;
