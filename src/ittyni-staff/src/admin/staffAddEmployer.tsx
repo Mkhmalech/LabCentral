09fb6b8abd87b0afc44285193adb6e8cb4d63b58
unknown <m.khmalech@gmail.com>
2020-04-11 01:06:11 +0100

    create form component

--- src/ittyni-staff/src/admin/staffAddEmployer.tsx
+++ src/ittyni-staff/src/admin/staffAddEmployer.tsx
@@ -2,115 +2,130 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import './style.css'
 
-export const StaffAddEmployer : React.FC<any> = ({username}) =>{
+export const StaffAddEmployer: React.FC<any> = ({ username }) => {
+
+  // create fileds hook
+  const [departement, setDepartement] = React.useState('')
+
+  const [departements, addDepartement] = React.useState([
+    "Biochimie",
+    "Hematologie",
+    "Bacteriologie",
+    "Serologie",
+  ]);
+
+  const [employerFName, add_employerFName] = React.useState<PersonFirstName>()
+  const [employerLName, add_employerLName] = React.useState<PersonLastName>()
+  const [employerCivility, add_employerCivility] = React.useState<PersonCivility>()
+  const [employerDepartement, add_employerDepartement] = React.useState<EmployerDepartement>()
+  const [employerPPR, add_employerPPR] = React.useState<EmployerPPR>()
+
+  const [hidden, setHidden] = React.useState(false);
+
+  const [employer, addEmployer] = React.useState<Employer>()
+
+  // form data 
+  const formData : FormFieldProps[]= [
+    {label : "Nom", placeholder : "nom", changeHandler : add_employerLName},
+    {label : "Prenom", placeholder : "Prenom", changeHandler : add_employerFName},
+    {label : "PPR", placeholder : "ppr", changeHandler : add_employerPPR, type : "number"}
+  ]
 
 
-    const [hidden, setHidden] = React.useState(false);
 
-    const hiddenSetHidden = (event: React.MouseEvent) => {
-        event.preventDefault()
-        setHidden(!hidden)
-    }
+  const hiddenSetHidden = (event: React.MouseEvent) => {
+    event.preventDefault()
+    setHidden(!hidden)
+  }
 
-    const [departement, setDepartement] = React.useState('')
+  
 
-    const [departements, addDepartement] = React.useState([
-        "Biochimie",
-        "Hematologie",
-        "Bacteriologie",
-        "Serologie", 
-    ])
+  const createEmployer = () => {
 
-    const addNewDepartement = (event: React.MouseEvent) => {
-      event.preventDefault();
+  }
 
-      if(departement)
+  const addNewDepartement = (event: React.MouseEvent) => {
+    event.preventDefault();
+
+    if (departement)
       addDepartement([
         ...departements,
         departement
-    ])
+      ])
     setHidden(!hidden)
-    }
+  }
 
-    return(
-        <div>
-            <h1>Ajouter un nouveau Employee</h1>
-            <Link to={'./list-all-employers'} > Returner </Link>
+  return (
+    <div>
+      <h1>Ajouter un nouveau Employee</h1>
+      <Link to={'./list-all-employers'} > Returner </Link>
 
-            <hr/>
+      <hr />
 
-            {/* here our form of data
-                we look for fullname, ppr, unite, status
-            */}
-<div className="form_wrapper">
-  <div className="form_container">
-    <div className="title_container">
-      <h2>Ajout de nouveau employer laboratoire</h2>
-      <h3>Information de l'employer</h3>
-    </div>
-    <form>
-
-    <div className="row clearfix">
-        <div className="col_half">
-          <label>Nom</label>
-          <div className="input_field">
-            <input type="text" name="nom" placeholder="Hicham"/>
+      {/* here our form of data
+        we look for fullname, ppr, unite, status
+      */}
+      <div className="form_wrapper">
+        <div className="form_container">
+          <div className="title_container">
+            <h2>Ajout de nouveau employer laboratoire</h2>
+            <h3>Information de l'employer</h3>
           </div>
-        </div>
-        <div className="col_half">
-          <label>Prenom</label>
-          <div className="input_field">
-            <input type="text" name="prenom" placeholder="Bendarbi" />
-          </div>
-        </div>
-      </div>
-      <div className="row clearfix">
-        <div className="col_half">
-          <label>PPR</label>
-          <div className="input_field">
-            <input type="number" name="ppr" placeholder="ppr" /> 
-          </div>
-        </div>
-        <div className="col_half">
-          <label>Cardre</label>
-          <div className="input_field">
-            <input type="text" name="cardre" placeholder="cardre" pattern="[0-9]{10}" />
-          </div>
-        </div>
-      </div>
-           
+          <form>
 
-        <div className="row clearfix">
-                         <div className="col_half">
-                            <label>Unite</label>
-                            <div className="test">
-                              <div className="select">
-                              <select name="slct" id="slct">
-                                  {departements.map(dep=><option>{dep}</option>)}
-                              </select>
-                              </div>
-                              <div className="container_btn ">
-                           {hidden && <button className="btns" onClick={event=> addNewDepartement(event)}>ok</button>}
-                          {!hidden && <button className="btns" onClick={event=>hiddenSetHidden(event)}>Ajouter Unite</button>}
-                           </div>
-                          </div>
-                          </div>
-                     
-       {hidden && <div className="col_half">
+            <div className="row clearfix">
+              {formData.map((field : FormFieldProps) =>
+                <FormField {...field} />
+              )}
+            </div>
+            <div className="row clearfix">
+              <div className="col_half">
+                <label>Unite</label>
+                <div className="test">
+                  <div className="select">
+                    <select name="slct" id="slct">
+                      {departements.map(dep => <option>{dep}</option>)}
+                    </select>
+                  </div>
+                  <div className="container_btn ">
+                    {hidden && <button className="btns" onClick={event => addNewDepartement(event)}>ok</button>}
+                    {!hidden && <button className="btns" onClick={event => hiddenSetHidden(event)}>Ajouter Unite</button>}
+                  </div>
+                </div>
+              </div>
+
+              {hidden && <div className="col_half">
                 <label>Nouvelle Unite</label>
-                    <div className="input_field">
-                 <input type="text"
-                  onChange={e=>{setDepartement(e.target.value) }}/>
+                <div className="input_field">
+                  <input type="text"
+                    onChange={e => { setDepartement(e.target.value) }} />
                 </div>
-                </div>
-      }
-           </div>
-            </form>
-            <input className="button" type="submit" value="Ajouter" />
-  </div>
-</div>
-            <h2> Les autorisations </h2>
-            <hr/>
+              </div>
+              }
+            </div>
+          </form>
+          <input className="button" value="Ajouter" onClick={createEmployer} />
         </div>
-    )
+      </div>
+      <hr />
+      <h2> Les autorisations </h2>
+      <hr />
+    </div>
+  )
 }
+
+interface FormFieldProps{
+  label : string
+  placeholder : string 
+  name? : string
+  type? : string
+  changeHandler : (e : any)=>void
+}
+const FormField : React.FC<FormFieldProps> = ({type , label, placeholder, name, changeHandler}) => (
+  <div className="col_half">
+    <label>{label}</label>
+    <div className="input_field">
+      <input type={type} name={name} placeholder={placeholder} onChange={(e:any)=>changeHandler(e.target.value)}/>
+    </div>
+  </div>
+)
